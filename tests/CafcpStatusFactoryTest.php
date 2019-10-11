7c75e7dcca85921e584b3f80e0303dd50d062360
Jesse Schneider <mechawrench@users.noreply.github.com>
2019-10-10 17:16:01 -0700

    Apply fixes from StyleCI (#1)

--- tests/CafcpStatusFactoryTest.php
+++ tests/CafcpStatusFactoryTest.php
@@ -3,13 +3,13 @@
 namespace Mechawrench\CafcpHydrogenStationFuelStatus\Tests;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
-use Mechawrench\CafcpHydrogenStationFuelStatus\CafcpHydrogenStationFuelStatus;
 use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Handler\MockHandler;
+use Mechawrench\CafcpHydrogenStationFuelStatus\CafcpHydrogenStationFuelStatus;
 
-class CafcpStatusFactory extends TestCase
+class CafcpStatusFactoryTest extends TestCase
 {
     /** @test */
     public function it_returns_a_station_status()
@@ -26,11 +26,11 @@ class CafcpStatusFactory extends TestCase
         $station_status = $stations->getStationStatus('Diamond Bar');
 
         $should_match = collect([
-            "station" => 'Diamond Bar',
-            "statusH70" => "offline",
-            "capacityH70" => "0 kg",
-            "statusH35" => "offline",
-            "capacityH35" => "0 kg"
+            'station' => 'Diamond Bar',
+            'statusH70' => 'offline',
+            'capacityH70' => '0 kg',
+            'statusH35' => 'offline',
+            'capacityH35' => '0 kg',
         ]);
 
         $this->assertEquals($station_status, $should_match);
@@ -46,23 +46,23 @@ class CafcpStatusFactory extends TestCase
         $this->assertArrayHasKey('station', $station_status);
     }
 
-      /** @test */
-      public function it_handles_station_name_missing_from_api()
-      {
-          $stations = new CafcpHydrogenStationFuelStatus();
-  
-          $station_status = $stations->getStationStatus('Fake Station here');
-  
-          $this->assertArrayHasKey('status', $station_status);
-      }
+    /** @test */
+    public function it_handles_station_name_missing_from_api()
+    {
+        $stations = new CafcpHydrogenStationFuelStatus();
 
-      /** @test */
-      public function it_uses_facade_to_get_data()
-      {
-          $stations = new CafcpHydrogenStationFuelStatus();
+        $station_status = $stations->getStationStatus('Fake Station here');
 
-          $station_status = CafcpHydrogenStationFuelStatus::getStationStatus('Diamond Bar');
-  
-          $this->assertArrayHasKey('station', $station_status);
-      }
+        $this->assertArrayHasKey('status', $station_status);
+    }
+
+    /** @test */
+    public function it_uses_facade_to_get_data()
+    {
+        $stations = new CafcpHydrogenStationFuelStatus();
+
+        $station_status = CafcpHydrogenStationFuelStatus::getStationStatus('Diamond Bar');
+
+        $this->assertArrayHasKey('station', $station_status);
+    }
 }
