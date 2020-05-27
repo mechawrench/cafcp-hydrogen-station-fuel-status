458c3ee795eedffd37a95169b961774105cf3588
mechawrench <mechawrench@users.noreply.github.com>
2020-05-27 22:58:50 +0000

    Fix styling

--- tests/CafcpStatusFactoryTest.php
+++ tests/CafcpStatusFactoryTest.php
@@ -26,10 +26,10 @@ class CafcpStatusFactoryTest extends TestCase
         $station_status = $stations->getStationStatus('Diamond Bar');
 
         $should_match = collect([
-            'station'     => 'Diamond Bar',
-            'statusH70'   => 'offline',
+            'station' => 'Diamond Bar',
+            'statusH70' => 'offline',
             'capacityH70' => '0 kg',
-            'statusH35'   => 'offline',
+            'statusH35' => 'offline',
             'capacityH35' => '0 kg',
         ]);
 
